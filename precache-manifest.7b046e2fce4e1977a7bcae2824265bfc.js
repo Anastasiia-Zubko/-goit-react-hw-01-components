be314af4bcd72fa4a1c03dc9f0bc4b8d0c93974a
Anastasiia-Zubko <anastasiia.zubko.8@gmail.com>
2022-02-07 12:53:58 +0200

    Updates

---
+++ precache-manifest.7b046e2fce4e1977a7bcae2824265bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c77194e93cdcceffe1efe521efff66",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "478866bb0ce7ff446271",
+    "url": "/goit-react-hw-01-components/static/css/main.478813e7.chunk.css"
+  },
+  {
+    "revision": "b1c5e685e7478249771b",
+    "url": "/goit-react-hw-01-components/static/js/2.1b9c6bc1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/goit-react-hw-01-components/static/js/2.1b9c6bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478866bb0ce7ff446271",
+    "url": "/goit-react-hw-01-components/static/js/main.deac61a4.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
